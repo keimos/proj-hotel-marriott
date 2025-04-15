0458afbab0f3178ab6ca7c94b017f99f05f55fb1
keithmosleyjr <keithmosleyjr@gmail.com>
2025-04-15 01:49:59 +0000

    Initial Push

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, requests, redirect, url_for, flash
+from werkzeug.exception import HTTPException
+import os
+
+app = Flask(__name__)
+
+if___name___ == "__main__":
+    app.run(debug=True)
