336a03256014298dd3067989e4a8165020423e58
keimos <keithmosleyjr@gmail.com>
2025-04-17 20:57:53 -0400

    Added reserve endpoint and template folder

--- app.py
+++ app.py
@@ -13,5 +13,29 @@ db.init_app(app)
 def create_tables():
         db.create_all()
 
-if___name___ == '__main__':
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/reserve', methods=['GET','POST'])
+def reserve():
+    form = ReservationForm()
+    if form.validate_on_submit():
+        try:
+             new_reservation = Reservation(
+                guest_name=form.guest_name.data,
+                room_type=form.room_type.data,
+                check_in_date=form.check_in_date.data,
+                check_out_date=form.check_out_date.data
+             )
+             db.session.add(new_reservation)
+             db.session.commit()
+             flash('Reservation successful!', 'success')
+             return redirect(url_for('success'))
+        except Exception as e:
+            db.session.rollback()
+            flash('Reservation failed. Please try again: {}'.format(str(e)), 'danger')
+    return render_template('reserve.html', form=form)
+
+if ___name___ == '__main__':
     app.run(debug=True)
