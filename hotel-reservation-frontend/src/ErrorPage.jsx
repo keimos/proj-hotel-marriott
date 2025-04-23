436b3bc09835aea8d3932900345f0de5f6d9fdb0
keithmosleyjr <keithmosleyjr@gmail.com>
2025-04-23 02:21:47 +0000

    Added Error Page

---
+++ hotel-reservation-frontend/src/ErrorPage.jsx
@@ -0,0 +1,10 @@
+function ErrorPage() {
+    return (
+    <div className="container">
+      <h2>Error Occurred</h2>
+      <p>Something went wrong. Please try again!</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
