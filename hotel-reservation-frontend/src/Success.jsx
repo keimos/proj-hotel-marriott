e87a0343032e51e7222fd288480a1117a39b568a
keimos <keithmosleyjr@gmail.com>
2025-04-28 23:20:54 -0400

    Added Success.jsx

---
+++ hotel-reservation-frontend/src/Success.jsx
@@ -0,0 +1,8 @@
+function Success() {
+    return (
+        <div className="container">
+            <h2>Reservation Successful!</h2>
+            <p>Your booking has been confirmed.</p>
+        </div>
+    );
+}
